3a070d6531f7f609bcbafcc53ca12dd392e36692
baychup <baychup@gmail.com>
2013-03-05 09:20:13 +0700

    baychup

---
+++ php/library/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$config['db']['host'] = '127.8.217.129';
+$config['db']['port'] = '3306';
+$config['db']['username'] = 'admin';
+$config['db']['password'] = '123456';
+$config['db']['dbname'] = 'forum';
+
+
+$config['superAdmins'] = '1';
